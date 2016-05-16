fe64a7d0107565630110c892f8a6602d2520e072
chiemy <luobaolong@gmail.com>
2016-05-16 22:19:37 +0800

    update

--- app/src/main/java/com/chiemy/example/databindingexample/ViewStubActivity.java
+++ app/src/main/java/com/chiemy/example/databindingexample/ViewStubActivity.java
@@ -1,7 +1,11 @@
 package com.chiemy.example.databindingexample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewStubProxy;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.*;
+import android.view.View;
 
 public class ViewStubActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class ViewStubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_stub);
+
+        ViewStubActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_view_stub);
+        final ViewStubProxy viewStubProxy = new ViewStubProxy(binding.viewStub);
+        viewStubProxy.setOnInflateListener(new ViewStub.OnInflateListener() {
+            @Override
+            public void onInflate(ViewStub stub, View inflated) {
+                InflatedLayoutBinding layoutBinding = (InflatedLayoutBinding)viewStubProxy.getBinding();
+                // TODO 为InflatedLayoutBinding设置数据
+            }
+        });
+        ...
+
+        // 必要时，填充进来
+        if(!viewStubProxy.isInflated()){
+            viewStubProxy.getViewStub().inflate();
+        }
     }
 }
